46bfdf8a02b3721233a999a5302e61a9559382bb
Tracy1819 <2425402105@live.gctu.edu.gh>
2026-05-29 11:27:14 +0100

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: <initials.py>
+# Description: <This program prints the initials of my name TBOY in large block letters made up of each letter's own character, followed by a large period made of asterisks.">
+# Assignment Number: 2
+#
+# Name: <TRACY BOATENG OHEMAA YAA>
+# STUDENT ID:  <2425402105>
+# Email: <2425402105@live.gctu.edu.gh>
+# Grader: <AUGUSTUS BUCKMAN>
+#
+# On my honor, <TRACY>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials TBOY in stylized large block letters using only print statements
 
+    print()
+    print("...TBOY")
+    print()
+    print("...TTTTTTTTTTTT........BBBBBBBBBB.............OOOOOO..........YY.........YY....")
+    print("...TTTTTTTTTTTT........BBBBBBBBBBB...........OO....OO...........YY......YY.....")
+    print(".......TTTT............BB....BBBBBB.........OO......OO...........YY....YY......")
+    print(".......TTTT............BB....BBBBBB........OO........OO............YYYY........")
+    print(".......TTTT............BBBBBBBBBBB.........OO........OO.............YY.........")
+    print(".......TTTT............BBBBBBBBBBB.........OO........OO.............YY.........")
+    print(".......TTTT............BB....BBBBBB........OO........OO.............YY.........")
+    print(".......TTTT............BB....BBBBBB.........OO......OO..............YY.........")
+    print(".......TTTT.......**...BBBBBBBBBBB...**......OO....OO.....**........YY.......**")
+    print(".......TTTT.......**...BBBBBBBBBB....**.......OOOOOO......**........YY.......**")
+    print()
 
 
 main()
